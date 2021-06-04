7a2da2e78117c267a18037917411012d1dacfea1
Danilo Malafaia <danilo.malafaia@nubank.com.br>
2021-06-04 15:30:02 -0300

    feat:Exposing all headers

--- Framework/AmplitudeFramework.h
+++ Framework/AmplitudeFramework.h
@@ -1,9 +1,21 @@
 // All Amplitude public headers
+#import <Amplitude/AMPBackgroundNotifier.h>
+#import <Amplitude/AMPNSURLSessionNetworkClient.h>
+#import <Amplitude/AMPEventUploadRequest.h>
+#import <Amplitude/AMPNetworkClient.h>
 #import <Amplitude/Amplitude.h>
 #import <Amplitude/AMPIdentify.h>
 #import <Amplitude/AMPRevenue.h>
+#import <Amplitude/AMPConfigManager.h>
 #import <Amplitude/AMPTrackingOptions.h>
+#import <Amplitude/AMPConstants.h>
+#import <Amplitude/AMPDatabaseHelper.h>
+#import <Amplitude/AMPDeviceInfo.h>
 #import <Amplitude/Amplitude+SSLPinning.h>
-#import <Amplitude/AMPEventUploadRequest.h>
-#import <Amplitude/AMPNSURLSessionNetworkClient.h>
-#import <Amplitude/AMPNetworkClient.h>
+#import <Amplitude/AmplitudePrivate.h>
+#import <Amplitude/AMPURLConnection.h>
+#import <Amplitude/AMPURLSession.h>
+#import <Amplitude/AMPUtils.h>
+#import <Amplitude/ISPCertificatePinning.h>
+#import <Amplitude/ISPPinnedNSURLConnectionDelegate.h>
+#import <Amplitude/ISPPinnedNSURLSessionDelegate.h>
